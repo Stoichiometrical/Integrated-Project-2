7c3d21ed897f9b01e6fc14f4918718a0814610a1
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2023-08-02 17:47:48 +0000

    Conatct form handling

--- joblog/src/components/contact-form/ContactForm.jsx
+++ joblog/src/components/contact-form/ContactForm.jsx
@@ -2,58 +2,77 @@ import React, { useState } from 'react';
 import "./contactform.scss"
 
 const ContactForm = () => {
-    const [formData, setFormData] = useState({
-        fullName: '',
-        email: '',
-        message: '',
-    });
+//     const [formData, setFormData] = useState({
+//         fullName: '',
+//         email: '',
+//         message: '',
+//     });
 
-    const [formErrors, setFormErrors] = useState({});
+//     const [formErrors, setFormErrors] = useState({});
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value,
-        }));
-    };
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevFormData) => ({
+//             ...prevFormData,
+//             [name]: value,
+//         }));
+//     };
 
-    const validateForm = () => {
-        const errors = {};
-        if (!formData.fullName.trim()) {
-            errors.fullName = 'Full Name is required';
-        }
-        if (!formData.email.trim()) {
-            errors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-            errors.email = 'Invalid email address';
-        }
-        if (!formData.message.trim()) {
-            errors.message = 'Message is required';
-        }
-        setFormErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
+//     const validateForm = () => {
+//         const errors = {};
+//         if (!formData.fullName.trim()) {
+//             errors.fullName = 'Full Name is required';
+//         }
+//         if (!formData.email.trim()) {
+//             errors.email = 'Email is required';
+//         } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+//             errors.email = 'Invalid email address';
+//         }
+//         if (!formData.message.trim()) {
+//             errors.message = 'Message is required';
+//         }
+//         setFormErrors(errors);
+//         return Object.keys(errors).length === 0;
+//     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            try {
-                const response = await fetch('https://formsubmit.co/d.gondo@alustudent.com', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(formData),
-                });
-                // Handle the response here (success or error)
-                console.log('Form submitted successfully!');
-            } catch (error) {
-                // Handle error here
-                console.error('Form submission error:', error);
-            }
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         if (validateForm()) {
+//             try {
+//                 const response = await fetch('https://formsubmit.co/d.gondo@alustudent.com', {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                     },
+//                     body: JSON.stringify(formData),
+//                 });
+//                 // Handle the response here (success or error)
+//                 console.log('Form submitted successfully!');
+//             } catch (error) {
+//                 // Handle error here
+//                 console.error('Form submission error:', error);
+//             }
+//         }
+//     };
+
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = () => {
+        if (fullName.length === 0){
+            alert("Please enter your Fullname");
         }
-    };
+        else if (email.length === 0){
+            alert("Please enter your email address")
+        }
+        else if (message.length === 0){
+            alert("Please write a message to us.")
+        }
+        else{
+            alert(`Thank you ${fullName}, we have received your message.`);
+        }
+    }
 
     return (
         
@@ -66,11 +85,9 @@ const ContactForm = () => {
                         id="fullName"
                         name="fullName"
                         placeholder='Enter your full name'
-                        value={formData.fullName}
-                        onChange={handleChange}
-                        className={formErrors.fullName ? 'error' : ''}
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
                     />
-                    {formErrors.fullName && <span className="error-message">{formErrors.fullName}</span>}
                 </div>
 
                 <div className="form-group">
@@ -80,11 +97,9 @@ const ContactForm = () => {
                         id="email"
                         name="email"
                         placeholder='Enter your email address'
-                        value={formData.email}
-                        onChange={handleChange}
-                        className={formErrors.email ? 'error' : ''}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
-                    {formErrors.email && <span className="error-message">{formErrors.email}</span>}
                 </div>
 
                 <div className="form-group">
@@ -93,14 +108,12 @@ const ContactForm = () => {
                         id="message"
                         name="message"
                         placeholder='Write your message...'
-                        value={formData.message}
-                        onChange={handleChange}
-                        className={formErrors.message ? 'error' : ''}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
                     />
-                    {formErrors.message && <span className="error-message">{formErrors.message}</span>}
                 </div>
                 <div>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
                 </div>
 
             </div>
